6b7f6ad94e0c551b3e237665b3f0cd7f916955be
devvbella <ava0712@naver.com>
2024-03-17 10:41:09 -0700

    crops added

--- transform.py
+++ transform.py
@@ -4,7 +4,7 @@ input_transform = standard_transforms.Compose([
         standard_transforms.RandomRotation(degrees = 10),
         standard_transforms.RandomHorizontalFlip(p=1),
         standard_transforms.RandomVerticalFlip(p=1), 
-        standard_transforms.ToTensor()
-        standard_transforms.RandomResizedCrop(size=(128, 128), scale=(0.8, 1.0)),
+        standard_transforms.ToTensor(),
+        standard_transforms.RandomResizedCrop(size=(128, 128), scale=(0.5, 0.75))
     ])
 
