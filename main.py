2dd1ced37b268543e375afd3185ad07131c658a9
TerryYan26 <103209100+TerryYan26@users.noreply.github.com>
2024-03-17 20:47:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,117 +1,73 @@
 import torch
-from torch import nn
-from tqdm import tqdm as progress_bar
-from arguments import params
-from utils import setup_gpus, set_seed
-from modelPrediction import PredictionModel
 from torch.utils.data import DataLoader
-import loadData
-from transformers import AutoImageProcessor, NatConfig,NatModel,NatForImageClassification
-
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-def train_prediction(args, model,img_processor):
-    print('training')
-    criterion = nn.CrossEntropyLoss()
+from tqdm import tqdm
+from arguments import params  # Ensure this matches the name of the file and function for your parameters.
+from utils import setup_gpus, set_seed  # Adjust according to your utilities for setting up GPUs and seeds.
+import loadData  # Adjust to your module for loading data.
+from modelPrediction import ConvNextClassification
+from transformers import AutoImageProcessor
+from modelPrediction import ConvNextClassification
+import os
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+def train_prediction(args, model, img_processor, train_loader, device):
+    print('Training...')
+    criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate, eps=args.adam_epsilon)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.8)
-    
-    radius = 16
-    radius_decay = 0.707107
-    
-    lst = []
-    losses =[]
-    acc = []
-    for epoch_count in range(args.n_epochs):
-        print("epoch",epoch_count)
-        loss = 0
-        
-        model.train()
 
-        for iter, (inputs, labels) in progress_bar(enumerate(train_loader),total=len(train_loader)):
-            model.zero_grad()
-            inputs = img_processor(inputs,return_tensors="pt")['pixel_values'].to(device)
+    model.train()
+    for epoch in range(args.n_epochs):
+        total_loss = 0
+        for batch in tqdm(train_loader, desc=f'Epoch {epoch}/{args.n_epochs}'):
+            inputs, labels = batch
+            inputs = img_processor(inputs, return_tensors="pt")['pixel_values'].to(device)
             labels = labels.to(device)
-            
-            logits = model(inputs).logits
-            
-            labels = labels.long()
-            labels = nn.functional.one_hot(labels, num_classes=10)
-            for label in labels:
-                idx = torch.argmax(label)
-                for i in range(idx - int(radius), idx + int(radius) + 1):
-                    if i >= 0 and i < len(label):
-                        label[i] = 1
 
-            labels = labels / labels.sum(dim = 1, keepdim=True)
-            
+            optimizer.zero_grad()
+            logits = model(inputs)
             loss = criterion(logits, labels)
             loss.backward()
-            losses += [loss.item()]
-
             optimizer.step()
 
+            total_loss += loss.item()
+
+        print(f'Epoch {epoch}: Loss {total_loss/len(train_loader)}')
         scheduler.step()
-        radius *= radius_decay
-        
-        val_acc,val_loss = run_eval(args,model,img_processor,test_loader)
-        lst += [val_loss]
-        acc += [val_acc]
-        print('epoch', epoch_count, 'loss:', loss)
-    
-    return lst,acc
-        
-def run_eval(args, model,img_processor,loader):
+
+def run_eval(args, model, img_processor, test_loader, device):
+    print('Evaluating...')
     model.eval()
-    criterion = nn.CrossEntropyLoss()
-        
-    losses ,acc = 0,0
-    for step, (inputs,labels) in progress_bar(enumerate(loader),total=len(loader)):
-        #print("inputs",inputs.size())
-        #print("labels",labels)
-        inputs = img_processor(inputs,return_tensors="pt")['pixel_values'].to(device)
-        labels = labels.to(device)
-        
-        #print("inputs 2",inputs['pixel_values'].size())
-        
-        with torch.no_grad():
-            logits = model(inputs).logits
-        
-        loss = criterion(logits,labels)
-        print("logits:", logits, "labels",labels,"preds",logits.argmax(1))
-        tem = (logits.argmax(1)==labels).float().sum()
-        acc += tem.item()
-        losses += loss.item()
-    
-    #print("loader size",len(loader))
-    return acc/len(loader), losses/len(loader)
-            
-        
+    criterion = torch.nn.CrossEntropyLoss()
+    total_loss, total_correct = 0, 0
+    with torch.no_grad():
+        for inputs, labels in tqdm(test_loader, desc='Evaluation'):
+            inputs = img_processor(inputs, return_tensors="pt")['pixel_values'].to(device)
+            labels = labels.to(device)
+            logits = model(inputs)
+            loss = criterion(logits, labels)
+            total_loss += loss.item()
+            total_correct += (logits.argmax(dim=-1) == labels).sum().item()
+
+    avg_loss = total_loss / len(test_loader)
+    accuracy = total_correct / len(test_loader.dataset)
+    print(f'Evaluation - Loss: {avg_loss}, Accuracy: {accuracy}')
+    return accuracy, avg_loss
+
 if __name__ == "__main__":
     args = params()
-    args = setup_gpus(args)
+    setup_gpus(args)
     set_seed(args)
-    
-    train_dataset = loadData.Dataset(args, 'train')
-    test_dataset = loadData.Dataset(args, 'test')
-    #print('train_dataset', train_dataset[0])
 
-    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    train_dataset = loadData.Dataset(args, 'train')  # Adjust loadData.Dataset to match your data loading approach.
+    test_dataset = loadData.Dataset(args, 'test')   # Adjust loadData.Dataset accordingly.
     train_loader = DataLoader(dataset=train_dataset, batch_size=args.batch_size, shuffle=True)
     test_loader = DataLoader(dataset=test_dataset, batch_size=args.batch_size, shuffle=False)
-    print(len(train_loader))
-    if args.task == 'prediction':
-        
-        # baseline NAT model
-        image_processor = AutoImageProcessor.from_pretrained("shi-labs/nat-mini-in1k-224")
-        
-        # model = PredictionModel(args).to(device)
-        model = NatForImageClassification.from_pretrained("shi-labs/nat-mini-in1k-224").to(device)
-        
-        train_prediction(args, model,image_processor)
-        
-    #     # top 1 accuracy
-        test_acc,test_loss = run_eval(args,model,image_processor,test_loader)
-        print(test_acc)
-    #     print("top 1 accuracy ",)
+
+    image_processor = AutoImageProcessor.from_pretrained("facebook/convnext-tiny-224")
+    model = ConvNextClassification(1000, "facebook/convnext-tiny-224").to(device)
+
+    train_prediction(args, model, image_processor, train_loader, device)
+    test_acc, test_loss = run_eval(args, model, image_processor, test_loader, device)
+    print(f'Final Test Accuracy: {test_acc}, Final Test Loss: {test_loss}')
